6364e1b1e141f95c67ad42cc45250a5ef2ca38d5
YeoBenShin <orang3cats@gmail.com>
2025-08-29 14:16:28 +0800

    feat: user can now upload video to the application TODO: send the video to the next screen

--- v2frontend/videoEditor/app/_layout.tsx
+++ v2frontend/videoEditor/app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+      <Stack.Screen name="index" options={{ title: "Home", headerShown: false }} />
+      <Stack.Screen name="editor" options={{ title: "Editor", headerShown: false }} />
+    </Stack>;
 }
--- v2frontend/videoEditor/app/index.tsx
+++ v2frontend/videoEditor/app/index.tsx
@@ -1,15 +1,54 @@
-import { Text, View } from "react-native";
+// import { Text, View } from "react-native";
+
+// export default function Index() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Upload Video</Text>
+//     </View>
+//   );
+// }
+
+
+import React, { useState } from "react";
+import { Button, View, Platform } from "react-native";
+import { Video } from "expo-av";
+import { router } from "expo-router";
+import * as ImagePicker from "expo-image-picker";
 
 export default function Index() {
+  const [videoUri, setVideoUri] = useState<string | null>(null);
+
+  const pickVideo = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1
+    });
+
+    if (!result.canceled) {
+      console.log("result:", result.assets[0].uri);
+      setVideoUri(result.assets[0].uri);
+      router.push("/editor");
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1}}>
+      <Button title="Upload Video" onPress={pickVideo} />
+      {videoUri && (
+        <Video
+          source={{ uri: videoUri }}
+          style={{ width: "100%", height: 300 }}
+          useNativeControls
+          // resizeMode="contain"
+        />
+      )}
     </View>
   );
 }
